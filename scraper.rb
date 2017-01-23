d055e1f9b08956d0ef5e7746b12c68d8061ecc0f
Lindsay Holmwood <lindsay@holmwood.id.au>
2017-01-23 16:56:57 +1100

    Fetch all animal types

--- scraper.rb
+++ scraper.rb
@@ -2,43 +2,45 @@ require 'scraperwiki'
 require 'mechanize'
 require 'pry'
 require 'reverse_markdown'
+require 'active_support/inflector'
 
 def get(url)
   @agent ||= Mechanize.new
   @agent.get(url)
 end
 
-def extract_listings(page)
-  page.search('li.dog-listing.listing').map do |listing|
+def extract_listings(page, type)
+  page.search("li.#{type}-listing.listing").map do |listing|
     {
-      'name' => listing.search('h4').text,
+      'name' => listing.search('h4').text.strip,
       'description' => listing.search('div.personality').text,
       'gender' => listing.search('dd.gender').first.text.downcase,
       'breed' => listing.search('dd.breed').first.text,
       'link' => 'https://www.petrescue.com.au' + listing.search('h4 a').first['href'],
-      'type' => 'dog',
+      'type' => type,
       'id'   => listing.search('h4 a').first['href'][/(\d+)$/, 1]
     }
   end
 end
 
-def fetch_animals(url)
-  page = get(url)
-  extract_listings(page)
-end
-
 def all_animals
   return @animals if @animals
 
-  puts "### [debug] Fetching animals index"
-  url = 'https://www.petrescue.com.au/listings/dogs?age=either&commit=Search&gender=either&page=1&postcode=&postcode_distance=50&size%5B%5D=all&species=dog&states%5B%5D=1&utf8=%E2%9C%93'
-  page = get(url)
-  max = page.search('#main > article > div.pagination.footer-pagination > nav > div.info').first.text.split.last.to_i
+  @animals = %w(other cats dogs).map { |type|
+    plural   = ActiveSupport::Inflector.pluralize(type)
+    singular = ActiveSupport::Inflector.singularize(type)
 
-  @animals = (1..max).to_a.map { |i|
-    puts "### [debug] Fetching page #{i} of #{max}"
-    url = "https://www.petrescue.com.au/listings/dogs?age=either&commit=Search&gender=either&page=#{i}&postcode=&postcode_distance=50&size%5B%5D=all&species=dog&states%5B%5D=1&utf8=%E2%9C%93"
-    fetch_animals(url)
+    puts "### [debug] Fetching #{plural} index"
+    url  = "https://www.petrescue.com.au/listings/#{type}"
+    page = get(url)
+    max  = page.search('#main > article > div.pagination.footer-pagination > nav > div.info').first.text.split.last.to_i
+
+    animals = (1..max).to_a.map { |i|
+      puts "### [debug] Fetching page #{i} of #{max} for #{plural}"
+      url  = "https://www.petrescue.com.au/listings/#{type}?page=#{i}"
+      page = get(url)
+      extract_listings(page, singular)
+    }.flatten
   }.flatten
 end
 
